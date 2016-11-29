4d5bb7338afe1071a2d09fe4326b6d3f317d4259
Huan Truong <htruong@tnhh.net>
2016-11-29 00:02:19 -0600

    Added initialization code and make the strip cycle through colors initially

--- arduino/stripbar/stripbar.ino
+++ arduino/stripbar/stripbar.ino
@@ -15,23 +15,37 @@
 // example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
+bool initialized = false;
+
 void setup() {
   // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-#if defined (__AVR_ATtiny85__)
+  // Pro Micro needs the clock_prescale_set to work. - Huan
+//#if defined (__AVR_ATtiny85__)
   if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-#endif
+//#endif
   // End of trinket special code
-
+  pixels.setBrightness(50);
   pixels.begin(); // This initializes the NeoPixel library.
+  pixels.show();
   Serial.begin(9600); //This pipes to the serial monitor
+  
 }
 
 void loop() {
 
+  
+  if (!initialized) {
+      rainbow(10);
+  }
+  
   // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
-
   // if there's any serial available, read it:
   while (Serial.available() > 0) {
+    if (!initialized) {
+        pixels.setBrightness(255);
+        pixels.show();
+        initialized = true;
+    }
 
     // look for the next valid integer in the incoming serial stream:
     int ledno = Serial.parseInt();
@@ -62,5 +76,49 @@ void loop() {
       pixels.show();
     }
   }
-
 }
+
+
+// Auxullary function
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256 * 5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< pixels.numPixels(); i++) {
+      pixels.setPixelColor(i, Wheel(((i * 256 / pixels.numPixels()) + j) & 255));
+    }
+    pixels.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<pixels.numPixels(); i++) {
+      pixels.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    pixels.show();
+    delay(wait);
+  }
+}
+
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3,0);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3,0);
+  }
+  WheelPos -= 170;
+  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0,0);
+}
+
